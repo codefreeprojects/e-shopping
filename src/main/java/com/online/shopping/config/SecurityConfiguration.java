c6c048f00ce58090e55f1cd5a0495ba36d70dd72
satish358 <satishmankar745@gmail.com>
2022-09-25 23:10:38 +0530

    Auth config

--- src/main/java/com/online/shopping/config/SecurityConfiguration.java
+++ src/main/java/com/online/shopping/config/SecurityConfiguration.java
@@ -40,7 +40,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/**")
+                .antMatchers("/api/auth/**")
                 .permitAll()
                 .antMatchers("/api/upload/**")
                 .permitAll()
